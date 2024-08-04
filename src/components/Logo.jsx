ce361b5127423895a94b64e44857c984f1509cb6
Himanshu Hardiya <himanshuhardiya18@gmail.com>
2024-08-04 09:56:36 +0530

    logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Logo({width = '100px'}) {
   return (
-    <div><img src="../assets/logo-icon.png" width="90px" alt="" srcset="" /></div>
+    <div><img src="../assets/logo-icon.png" width="90px" alt="Logo" srcset="" /></div>
   )
 }
 
